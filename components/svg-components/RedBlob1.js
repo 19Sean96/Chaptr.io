4ce7f37282dd25ed1748c911877c4980b68c0c1b
Sean <1996.sean.alexander@gmail.com>
2020-07-10 02:53:21 -0700

    added par-4 & black bg. began svg path anim for image 2

--- components/svg-components/RedBlob1.js
+++ components/svg-components/RedBlob1.js
@@ -1,12 +1,22 @@
-export default () => (
-  <svg
-    className="red-blob red-blob-1"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 415.079 417.426"
-  >
-    <path
-      d="M100.5,0C240.117-49.747,328.56-.379,289.275,80.71S127.643,244.654,74.787,223.562,86.131,100.753,0,100.5-39.117,49.747,100.5,0Z"
-      transform="matrix(-0.695, 0.719, -0.719, -0.695, 370.663, 188.741)"
-    />
-  </svg>
-);
+import { useState } from "react";
+export default (props) => {
+  const [hovered, setHover] = useState(false);
+  return (
+    <svg
+      className={`red-blob red-blob-${props.num}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 415.079 417.426"
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+      id={props.id}
+    >
+      <path
+        d="M100.5,0C240.117-49.747,328.56-.379,289.275,80.71S127.643,244.654,74.787,223.562,86.131,100.753,0,100.5-39.117,49.747,100.5,0Z"
+        transform={!hovered ?
+          "matrix(-0.695, 0.719, -0.719, -0.695, 370.663, 188.741)"
+          : "matrix(-1.195, 0.419, -1.719, -0.295, 470.663, 238.741)"
+        }
+      />
+    </svg>
+  );
+};
